adc02e5477f24c5c35e36aa0840f26100eeaf626
rupesh1310 <a.rupesh66@gmail.com>
2019-03-08 20:49:16 +0530

    sidebar

--- src/containers/SideBar/SideBarItem/SideBarItem.js
+++ src/containers/SideBar/SideBarItem/SideBarItem.js
@@ -6,12 +6,19 @@ export function SideBarItem(props) {
   // React will ignore custom boolean attributes, therefore we pass a string
   // we use this attribute in our SCSS for styling
   const highlight = props.highlight ? 'highlight-item' : null;
+  render() {
   return (
-      <Menu.Item className={['sidebar-item', highlight].join(' ')}>
-        <div className='sidebar-item-alignment-container'>
-          <span><Icon size='large' name={props.icon}/> </span>
-          <span>{props.label}</span>
-        </div>
-      </Menu.Item>
+    <Menu borderless vertical stackable fixed='left' className='side-nav'>
+      <SideBarItem highlight={true}  label='Home' icon='home'/>
+      <SideBarItem label='Trending' icon='fire'/>
+      <SideBarItem label='Followers' icon='spy'/>
+      <SideBarItem label='History' icon='history'/>
+      <SideBarItem label='Watch later' icon='clock'/>
+      <SideBarItem label='Liked videos' icon='thumbs up'/>
+      <SideBarItem label='Movies and Shows' icon='film'/>
+      <SideBarItem label='Report history' icon='flag'/>
+      <SideBarItem label='Help' icon='help circle'/>
+      <SideBarItem label='Send feedback' icon='comment'/>
+    </Menu>
   );
 }
